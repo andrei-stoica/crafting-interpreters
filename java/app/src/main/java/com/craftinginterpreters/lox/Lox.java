a9ea62988888ad6b69c16b63b9e56b74d3dd746c
Andrei Stoica <andrei.stoica.365@gmail.com>
2024-04-08 23:07:37 -0400

    added java project with lexer

---
+++ java/app/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,67 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+	static boolean hadError = false;
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("test");
+		if (args.length > 1) {
+			System.out.println("Usage: jlox [script]");
+		} else if (args.length == 1) {
+			runFile(args[0]);
+		} else {
+			runPrompt();
+		}
+	}
+
+	public static void runFile(String path) throws IOException {
+		byte[] bytes = Files.readAllBytes(Paths.get(path));
+		run(new String(bytes, Charset.defaultCharset()));
+		if (hadError) {
+			System.exit(65);
+		}
+	}
+
+	public static void runPrompt() throws IOException {
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input);
+
+		for (;;) {
+			System.out.print("> ");
+			String line = reader.readLine();
+			if (line == null) {
+				break;
+			}
+			run(line);
+			hadError = false;
+		}
+
+	}
+
+	public static void run(String source) {
+		Scanner scanner = new Scanner(source);
+		List<Token> tokens = scanner.scanTokens();
+
+		for (Token token : tokens) {
+			System.out.println(token);
+		}
+
+	}
+
+	public static void error(int line, String message) {
+		report(line, "", message);
+	}
+
+	public static void report(int line, String where, String message) {
+		System.out.println("[line " + line + "] Error" + where + ": " + message);
+	}
+}
