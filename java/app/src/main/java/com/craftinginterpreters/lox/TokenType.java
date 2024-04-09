6b48420a7f5f6495a01ad575848b4cf595e7aa4c
Andrei Stoica <andrei.stoica.365@gmail.com>
2024-04-09 12:55:01 -0400

    formating

--- java/app/src/main/java/com/craftinginterpreters/lox/TokenType.java
+++ java/app/src/main/java/com/craftinginterpreters/lox/TokenType.java
@@ -1,20 +1,51 @@
 package com.craftinginterpreters.lox;
 
 enum TokenType {
-	// Single-char tokens
-	LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-	COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+  // Single-char tokens
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  COMMA,
+  DOT,
+  MINUS,
+  PLUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
 
-	// one or more char tokens
-	BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
-	GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+  // one or more char tokens
+  BANG,
+  BANG_EQUAL,
+  EQUAL,
+  EQUAL_EQUAL,
+  GREATER,
+  GREATER_EQUAL,
+  LESS,
+  LESS_EQUAL,
 
-	// literals
-	IDENTIFIER, STRING, NUMBER,
+  // literals
+  IDENTIFIER,
+  STRING,
+  NUMBER,
 
-	// keywords
-	AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-	PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+  // keywords
+  AND,
+  CLASS,
+  ELSE,
+  FALSE,
+  FUN,
+  FOR,
+  IF,
+  NIL,
+  OR,
+  PRINT,
+  RETURN,
+  SUPER,
+  THIS,
+  TRUE,
+  VAR,
+  WHILE,
 
-	EOF
+  EOF
 }
